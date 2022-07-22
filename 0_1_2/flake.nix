1addd41bf57f1c117d68fabc38f74e674940e220
hugosenari <hugosenari@gmail.com>
2022-07-21 22:17:12 -0300

    chore: re index alea

---
+++ 0_1_2/flake.nix
@@ -0,0 +1,37 @@
+{
+  description = ''A library to work with random variables'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-alea-0_1_2.flake = false;
+  inputs.src-alea-0_1_2.ref   = "refs/tags/0.1.2";
+  inputs.src-alea-0_1_2.owner = "andreaferretti";
+  inputs.src-alea-0_1_2.repo  = "alea";
+  inputs.src-alea-0_1_2.type  = "github";
+  
+  inputs."random".owner = "nim-nix-pkgs";
+  inputs."random".ref   = "master";
+  inputs."random".repo  = "random";
+  inputs."random".dir   = "v0_5_7";
+  inputs."random".type  = "github";
+  inputs."random".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."random".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-alea-0_1_2"];
+    over = if builtins.pathExists ./override.nix 
+           then { override = import ./override.nix; }
+           else { };
+  in lib.mkRefOutput (over // {
+    inherit self nixpkgs ;
+    src  = deps."src-alea-0_1_2";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  } );
+}
